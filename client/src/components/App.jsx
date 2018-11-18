836ebdba38fc4c3ada00f5835c582fcaed74d9df
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-11-17 19:51:47 -0600

    init setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 //import axios from 'axios';
 //IMPORT OTHER COMPONENTS
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+const App = () => (
+  <div>
+    <div className="menu_bar_container">
+      HELLO WORLD
+    </div>   
+  </div>
+);
 
-    //ADD BINDINGS HERE
-  }
 
-  render() {
-    return (
-      <div className="container">
-        HELLO WORLD!!!!
-      </div>
-    );
-  }
-}
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+
+//     //ADD BINDINGS HERE
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         HELLO WORLD!!!!
+//       </div>
+//     );
+//   }
+// }
+
 
 export default App;
